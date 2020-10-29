75add838fac0ffdd79a4980c2e2f23cfa37b193c
RashiniKaweesha <rashinikaweesha47@gmail.com>
2020-10-29 14:09:23 +0530

    solution for arithmatics operetions

---
+++ operators/Arithmatic_solution.py
@@ -0,0 +1,14 @@
+# Program to find the sum of all numbers stored in a list
+
+# input values for number 1 and number 2
+num1=int(input('Eneter First Number'))
+num2=int(input('Eneter Second Number'))
+
+# variable to store the sum
+sum = 0
+
+#calculation of input numbers
+sum=num1+num2
+
+#print the output
+print("The sum is", sum)
